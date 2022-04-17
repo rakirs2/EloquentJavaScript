1d7e4f8249e55263500559585fba6a27da55e7ef
rakirs <srikar.mylavarapu@gmail.com>
2022-04-17 13:25:36 -0500

    working on alist

--- Chapter4/aList.js
+++ Chapter4/aList.js
@@ -1,6 +1,45 @@
-function arrayToList(){
+function arrayToList(array){
+    let list = {}
+    let pointer = list
+    for (let i = 0; i <array.length; i++){
+        pointer["value"]= array[i]
+        if (i!= array.length-1){
+            pointer["rest"]= {}
+        }
+        else{
+            pointer["rest"]= null
+        }
+        pointer = pointer["rest"]
+    }
+    return list
+}
+function listToArray(list){
+    array = []
+    temp = list
+    while (temp.rest != null){
+        array.push(temp["value"])
+        temp = temp.rest
+    }
+    if (temp["value"]){
+        array.push(temp["value"])
+    }
+    return array
+}
+function prepend(list, value){
+  return {
+    "value": value, 
+    "rest": list}
 
 }
-function listToArray(){
-    
+
+function nth(list, value){
+
 }
+
+function nthRecursive(list,value){
+
+}
+
+let list = arrayToList([2,3])
+console.log(listToArray(arrayToList([1,2,3])))
+console.log(prepend(list,1))
