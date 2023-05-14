283e45cf5015b2f446244fead75bf0527ace2f28
Long Tran <long.tran1065@gmail.com>
2023-05-14 09:45:59 -0500

    looping a triangle homework

--- LT/submissions/02/loopingATriangle.js
+++ LT/submissions/02/loopingATriangle.js
@@ -1,1 +1,10 @@
-console.log("looping a triangle")
+
+
+let output = ''
+for (let number = 1; number <= 7; number  = number + 1 ){
+    // we have the right number
+    // can we make a function that goes from to the right number of #
+    output = output+ "#"
+    console.log(output)
+    // keep track of values here
+}
