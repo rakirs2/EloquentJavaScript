9a37550b9bc6dd8b5fb117c0347445e2ed204ae4
Long Tran <long.tran1065@gmail.com>
2023-05-29 09:04:12 -0500

    fizzBuzz.js

--- LT/submissions/02/fizzBuzz.js
+++ LT/submissions/02/fizzBuzz.js
@@ -1,1 +1,8 @@
-console.log("fizz buzz")
+
+
+for (x=1; x <= 100; x++) {
+    let output ="";
+    if (x % 3 == 0) output += "Fizz";
+    if (x % 5 == 0) output += "Buzz";
+    console.log(output || x);
+}
---
+++ LT/submissions/02/loopingATriangleRedo.js
@@ -0,0 +1,6 @@
+
+
+let pound = ''
+for (let number = 1; number <=7; number = number+ 1){
+pound = pound + '#'
+console.log(pound)}
