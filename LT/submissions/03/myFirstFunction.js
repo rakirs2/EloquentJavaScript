79aeabb69fc84ab41dfd024e7c2c9963628fa1cc
Long Tran <long.tran1065@gmail.com>
2023-05-29 09:47:02 -0500

    myFirstFunction.js

---
+++ LT/submissions/03/myFirstFunction.js
@@ -0,0 +1,11 @@
+const greeting = function(firstName) {
+    console.log("My name is "+firstName)
+};
+
+greeting("Long");
+greeting("Srikar")
+
+// Greeting si the name of the funciton
+// firstName is abstract, it is a prarameter 
+// line 2 is the body of the function
+// Long & Srikar are the argument
