5e434fba4458a2000ec01d28d2e2326593d55b95
Long Tran <long.tran1065@gmail.com>
2023-06-07 20:32:30 -0500

    LT/submissions/03/multiplcationTable.js

---
+++ LT/submissions/03/multiplcationTable.js
@@ -0,0 +1,17 @@
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * 5;
+
+    // display the result
+    console.log(`${5} * ${i} = ${result}`);
+}
+
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * 10;
+
+    // display the result
+    console.log(`${10} * ${i} = ${result}`);
+}
