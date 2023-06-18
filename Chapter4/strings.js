4a0e58bb7c11b636faaa11a865531ad1df72a11d
Long Tran <long.tran1065@gmail.com>
2023-06-18 09:49:15 -0500

    strings.js

---
+++ Chapter4/strings.js
@@ -0,0 +1,7 @@
+let yourName = 'Long'
+yourName.charAt(0);
+console.log(yourName.charAt(0));
+console.log(yourName.charCodeAt(0));
+console.log(yourName.codePointAt(0));
+console.log(yourName.slice(0,2));
+console.log(yourName.indexOf('n'));
